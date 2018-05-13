984e3f6dbcbc835f5ecbe022b5913f9d5d04eb4f
Mikač Martin <martin.mikac@kkkv.cz>
2018-05-13 18:38:43 +0200

    cmt

--- module/Application/src/Controller/IndexController.php
+++ module/Application/src/Controller/IndexController.php
@@ -14,12 +14,13 @@ use Zend\View\Model\JsonModel;
 use Bluerhinos\phpMQTT;
 
 class IndexController extends AbstractActionController {
+    
+    public $stavySenzoru = array();
 
     public function indexAction() {
         return new ViewModel();
     }
 
-    
     /**
      * The "ALERT" action displays the info about currently logged in user.
      */
@@ -28,31 +29,76 @@ class IndexController extends AbstractActionController {
         $request = $this->getRequest();
         $query = $request->getQuery();
 
+
+        $server = "m21.cloudmqtt.com";     // change if necessary
+        $port = 14212;                     // change if necessary
+        $username = "ozgbbnce";                   // set your username
+        $password = "7APK8RexzIGr";                   // set your password
+        $client_id = "Miki_007-Home_sokolov"; // make sure this is unique for connecting to sever - you could use uniqid()        
+
+
+
         if ($request->isXmlHttpRequest() || $query->get('showJson') == 1) {
 
 
+
+            $mqtt = new phpMQTT($server, $port, $client_id);
+
+            if (!$mqtt->connect(true, NULL, $username, $password)) {
+                exit(1);
+            }
+
+            $topics['kvetiny/senzor'] = array("qos" => 0, "function" => "procmsg");
+            $mqtt->subscribe($topics, 0);
+
+            /*while ($mqtt->proc()) {
+                if (array_count_values($this->stavySenzoru) > 0){
+                    break;
+                }
+                print_r($this->stavySenzoru);
+            }
+            */
+
+            $mqtt->close();
+
             $jsonData = array();
             $idx = 0;
-            //
-            //            foreach ($data as $sampledata) {
-            $temp = array(
+//
+//            foreach ($data as $sampledata) {
+            /*
+              $temp = array(
                 'isAlert' => "true",
                 'balkon_1p' => "34,5",
                 'cele_jmeno' => "Novák",
                 'umisteni' => "Čítárna",
                 'telefon' => "723 027 278"
             );
-            $jsonData[$idx++] = $temp;
-            //            }
-            //$view = new JsonModel($alerts);
+              
+             $jsonData[$idx++] = $temp;
+             
+             */
             
+            
+            
+            $jsonData[$idx++] = $this->stavySenzoru;
+//            }
+//$view = new JsonModel($alerts);
+
             $view = new JsonModel($jsonData);
             $view->setTerminal(true);
         } else {
-                    $view = new ViewModel();
+            $view = new ViewModel();
         }
         return $view;
-    }    
+    }
 
-    
+    function procmsg($topic, $msg) {
+        echo "Msg Recieved: " . date("r") . "\n";
+        echo "Topic: {$topic}\n\n";
+        echo "\t$msg\n\n";
+        $this->stavySenzoru = array(
+          $topic => $msg
+        );
+    }
+
 }
